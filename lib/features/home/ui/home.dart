52c610094bc9e6d4d5fb0dcf6b23c0c2de7099a9
Devina <devtjandra@gmail.com>
2020-11-07 12:33:07 +0800

    Created folder structure

---
+++ lib/features/home/ui/home.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Counter App"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              'test',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => {},
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
---
+++ lib/models/counter.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Counter {
+  final String title;
+  final int count;
+
+  Counter(this.title, @required this.count);
+}
